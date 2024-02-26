95a8e0f1690e2db24ca39b41a9607cbb29d7c1f9
abeermosaad <abeermosaad69@gmail.com>
2024-02-27 00:05:32 +0200

    [edit] Task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
-  process.exit();
 });
 
-process.on('exit', () => {
-  console.log('This important software is now closing\n');
+process.stdin.on('end', () => {
+  console.log('This important software is now closing');
 });
