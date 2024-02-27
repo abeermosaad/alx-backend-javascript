4a0fa74a48aba7698954c4833130496c86946dbb
abeermosaad <abeermosaad69@gmail.com>
2024-02-27 22:57:54 +0200

    [Edit] Task 4 again

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -2,7 +2,9 @@ const http = require('http');
 
 const port = 1245;
 const app = (req, res) => {
-  res.writeHead(200);
+  res.writeHead(200, {
+    'Content-Type': 'text/plain',
+  });
   res.end('Hello Holberton School!');
 };
 
