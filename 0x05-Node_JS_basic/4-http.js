9a84ce95030f257f14b1f0de29f71ecc4ac07fd6
abeermosaad <abeermosaad69@gmail.com>
2024-02-27 23:11:37 +0200

    [Edit] Task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,6 +1,8 @@
 const http = require('http');
 
 const port = 1245;
+const host = 'localhost';
+
 const app = (req, res) => {
   res.writeHead(200, {
     'Content-Type': 'text/plain',
@@ -9,4 +11,8 @@ const app = (req, res) => {
 };
 
 const server = http.createServer(app);
-server.listen(port);
+server.listen(port, host, () => {
+  console.log(`Server is running on http://${host}:${port}`);
+});
+
+module.exports = app;
