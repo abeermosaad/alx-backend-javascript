667e26fe60f89bf8ccd064df6d10230b18509e95
abeermosaad <abeermosaad69@gmail.com>
2024-03-02 03:22:54 +0200

    [Update] with more test cases

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -4,19 +4,65 @@ const assert = require('assert');
 
 describe('calculateNumber func', function(){
     
-    it('should return 4', function(){
-        assert.equal(calculateNumber(1, 3), 4)
+    it('Positive Numbers: int and int', function(){
+        const result = calculateNumber(1, 3);
+        assert.equal(result, 4)
     })
     
-    it('should return 5', function(){
-        assert.equal(calculateNumber(1, 3.7), 5)
+    it('Positive Numbers: int and float, in order', function(){
+        const result = calculateNumber(1, 3.14);
+        assert.equal(result, 4)
+    })
+    
+    it('Positive Numbers: flaot and int, in order', function(){
+        const result = calculateNumber(1.67, 3);
+        assert.equal(result, 5)
+    })
+    
+    it('Positive Numbers: flaot and float', function(){
+        const result = calculateNumber(1.45, 3.14);
+        assert.equal(result, 4)
+    })
+    
+    it('Positive Numbers: flaot and float', function(){
+        const result = calculateNumber(1.578, 3.678);
+        assert.equal(result, 6)
+    })
+    
+    it('Positive Numbers: flaot and float', function(){
+        const result = calculateNumber(0.4, 0.6);
+        assert.equal(result, 1)
     })
 
-    it('should return 5', function(){
-        assert.equal(calculateNumber(1.3, 3.5), 5)
+    it('Negative Numbers: int and int', function(){
+        const result = calculateNumber(-1, -3);
+        assert.equal(result, -4)
     })
-
-    it('should return 6', function(){
-        assert.equal(calculateNumber(1.5, 3.7), 6)
+    
+    it('Negative Numbers: int and float, in order', function(){
+        const result = calculateNumber(1, -3.14);
+        assert.equal(result, -2)
     })
+    
+    it('Negative Numbers: flaot and int, in order', function(){
+        const result = calculateNumber(-1.67, 3);
+        assert.equal(result, 1)
+    })
+    
+    it('Negative Numbers: flaot and float', function(){
+        const result = calculateNumber(-1.45, -3.14);
+        assert.equal(result, -4)
+    })
+    
+    it('Negative Numbers: flaot and float', function(){
+        const result = calculateNumber(-1.578, -3.678);
+        assert.equal(result, -6)
+    })
+    
+    it('Negative Numbers: flaot and float', function(){
+        const result = calculateNumber(0.4, -0.6);
+        assert.equal(result, -1)
+    })
+    
+    
 })
